e5d1af59498d64f33e18584ae0b21b98fba61810
usmanovbf <usmanovbf@gmail.com>
2019-05-18 11:11:17 +0300

    deleted cloud spring

--- src/main/java/com/stdstack/service/course/courseservice/CourseServiceApplication.java
+++ src/main/java/com/stdstack/service/course/courseservice/CourseServiceApplication.java
@@ -2,11 +2,9 @@ package com.stdstack.service.course.courseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableSwagger2
 public class CourseServiceApplication {
 
