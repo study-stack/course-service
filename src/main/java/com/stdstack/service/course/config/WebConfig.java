02e32b33c246ce32c67cbf46263094f3788f702c
usmanovbf <usmanovbf@gmail.com>
2019-05-19 17:56:47 +0300

    added cors settings

--- src/main/java/com/stdstack/service/course/config/WebConfig.java
+++ src/main/java/com/stdstack/service/course/config/WebConfig.java
@@ -1,9 +1,18 @@
 package com.stdstack.service.course.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*")
+                .allowedHeaders("*");
+    }
 
 }
